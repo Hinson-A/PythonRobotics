d6f6939af1f3844ed938231e22f8da49f338bd91
liucanglong@shenhaoinfo.com <liucanglong@shenhaoinfo.com>
2021-12-11 23:05:24 +0800

    add comments dijkstra.py

--- PathPlanning/Dijkstra/dijkstra.py
+++ PathPlanning/Dijkstra/dijkstra.py
@@ -1,22 +1,15 @@
-"""
-
-Grid based Dijkstra planning
-
-author: Atsushi Sakai(@Atsushi_twi)
-
-"""
-
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-  
 import matplotlib.pyplot as plt
 import math
 
 show_animation = True
 
-
 class Dijkstra:
 
     def __init__(self, ox, oy, resolution, robot_radius):
         """
-        Initialize map for a star planning
+        Initialize map for planning
 
         ox: x position list of Obstacles [m]
         oy: y position list of Obstacles [m]
@@ -34,6 +27,7 @@ class Dijkstra:
 
         self.resolution = resolution
         self.robot_radius = robot_radius
+        #构建栅格地图
         self.calc_obstacle_map(ox, oy)
         self.motion = self.get_motion_model()
 
@@ -41,8 +35,9 @@ class Dijkstra:
         def __init__(self, x, y, cost, parent_index):
             self.x = x  # index of grid
             self.y = y  # index of grid
-            self.cost = cost
-            self.parent_index = parent_index  # index of previous Node
+            self.cost = cost  # g(n)
+            self.parent_index = parent_index  
+            # index of previous Node
 
         def __str__(self):
             return str(self.x) + "," + str(self.y) + "," + str(
@@ -62,20 +57,23 @@ class Dijkstra:
             rx: x position list of the final path
             ry: y position list of the final path
         """
-
+        #将起点和终点转换为节点形式，即包含下标，代价值和父节点信息
         start_node = self.Node(self.calc_xy_index(sx, self.min_x),
-                               self.calc_xy_index(sy, self.min_y), 0.0, -1)
+                               self.calc_xy_index(sy, self.min_y), 0.0, -1)   
+                               # round((position - minp) / self.resolution)
         goal_node = self.Node(self.calc_xy_index(gx, self.min_x),
                               self.calc_xy_index(gy, self.min_y), 0.0, -1)
 
-        open_set, closed_set = dict(), dict()
+        open_set, closed_set = dict(), dict()     # key - value: hash表
+        #key是表示索引，value 是节点信息
         open_set[self.calc_index(start_node)] = start_node
 
         while 1:
-            c_id = min(open_set, key=lambda o: open_set[o].cost)
+            c_id = min(open_set, key=lambda o: open_set[o].cost)  
+            # 取cost最小的节点
             current = open_set[c_id]
 
-            # show graph
+            # show graph  动画仿真
             if show_animation:  # pragma: no cover
                 plt.plot(self.calc_position(current.x, self.min_x),
                          self.calc_position(current.y, self.min_y), "xc")
@@ -86,6 +84,7 @@ class Dijkstra:
                 if len(closed_set.keys()) % 10 == 0:
                     plt.pause(0.001)
 
+            # 判断是否是终点
             if current.x == goal_node.x and current.y == goal_node.y:
                 print("Find goal")
                 goal_node.parent_index = current.parent_index
@@ -121,7 +120,7 @@ class Dijkstra:
         rx, ry = self.calc_final_path(goal_node, closed_set)
 
         return rx, ry
-
+    #通过父节点追溯从起点到终点的最佳路径 
     def calc_final_path(self, goal_node, closed_set):
         # generate final course
         rx, ry = [self.calc_position(goal_node.x, self.min_x)], [
@@ -143,8 +142,8 @@ class Dijkstra:
         return round((position - minp) / self.resolution)
 
     def calc_index(self, node):
-        return (node.y - self.min_y) * self.x_width + (node.x - self.min_x)
-
+        return node.y * self.x_width + node.x
+    #检查是否超出地图范围或有障碍物
     def verify_node(self, node):
         px = self.calc_position(node.x, self.min_x)
         py = self.calc_position(node.y, self.min_y)
@@ -158,13 +157,13 @@ class Dijkstra:
         if py >= self.max_y:
             return False
 
-        if self.obstacle_map[node.x][node.y]:
+        if self.obstacle_map[int(node.x)][int(node.y)]:
             return False
 
         return True
 
     def calc_obstacle_map(self, ox, oy):
-
+        ''' 第1步：构建栅格地图 '''
         self.min_x = round(min(ox))
         self.min_y = round(min(oy))
         self.max_x = round(max(ox))
@@ -174,19 +173,24 @@ class Dijkstra:
         print("max_x:", self.max_x)
         print("max_y:", self.max_y)
 
+        #计算X和Y方向 栅格的个数
         self.x_width = round((self.max_x - self.min_x) / self.resolution)
         self.y_width = round((self.max_y - self.min_y) / self.resolution)
         print("x_width:", self.x_width)
         print("y_width:", self.y_width)
 
         # obstacle map generation
-        self.obstacle_map = [[False for _ in range(self.y_width)]
-                             for _ in range(self.x_width)]
-        for ix in range(self.x_width):
+        # 初始化地图，地图是要用二维向量表示的，在这里采用两层列表来表示
+        #初始化为false，内层为Y方向栅格的个数，外层为X方向栅格个数
+        self.obstacle_map = [[False for _ in range(int(self.y_width))]
+                             for _ in range(int(self.x_width))]
+        # 设置障碍物
+        for ix in range(int(self.x_width)):
             x = self.calc_position(ix, self.min_x)
-            for iy in range(self.y_width):
+            for iy in range(int(self.y_width)):
                 y = self.calc_position(iy, self.min_y)
                 for iox, ioy in zip(ox, oy):
+                    #障碍物到栅格的距离，如果小于车体半径，就标记为true,障碍物膨胀
                     d = math.hypot(iox - x, ioy - y)
                     if d <= self.robot_radius:
                         self.obstacle_map[ix][iy] = True
@@ -195,6 +199,7 @@ class Dijkstra:
     @staticmethod
     def get_motion_model():
         # dx, dy, cost
+        #定义机器人行走的代价，以当前点为中心周围八个栅格的代价
         motion = [[1, 0, 1],
                   [0, 1, 1],
                   [-1, 0, 1],
@@ -206,19 +211,19 @@ class Dijkstra:
 
         return motion
 
-
 def main():
-    print(__file__ + " start!!")
-
     # start and goal position
     sx = -5.0  # [m]
     sy = -5.0  # [m]
     gx = 50.0  # [m]
     gy = 50.0  # [m]
-    grid_size = 2.0  # [m]
-    robot_radius = 1.0  # [m]
+    #地图上的每一格栅格大小
+    grid_size = 3  # [m]
+    #机器人的半径
+    robot_radius = 0.5  # [m]
 
     # set obstacle positions
+    #设置障碍物区域
     ox, oy = [], []
     for i in range(-10, 60):
         ox.append(i)
@@ -254,6 +259,7 @@ def main():
         plt.pause(0.01)
         plt.show()
 
-
 if __name__ == '__main__':
     main()
+
+
